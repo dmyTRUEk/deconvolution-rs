650a4edb78cc3e2a33a82516449376a28ef00d3d
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-04-20 00:43:23 +0300

    fix: `SatExp_TwoDecExp_SeparateConsts`

--- src/deconvolution/types/sat_exp__two_dec_exp__separate_consts.rs
+++ src/deconvolution/types/sat_exp__two_dec_exp__separate_consts.rs
@@ -10,7 +10,7 @@ use crate::{
     extensions::ToStringWithSignificantDigits,
     load::Load,
     stacktrace::Stacktrace,
-    types::{float::float, named_wrappers::{Deconvolved, DeconvolvedV, Params, ParamsG, ParamsV}},
+    types::{float::float, linalg::DVect, named_wrappers::{Deconvolved, DeconvolvedV, Params, ParamsG, ParamsV}},
     utils_io::format_by_dollar_str,
 };
 
@@ -74,6 +74,15 @@ pub struct InitialValues_SatExp_TwoDecExp_SeparateConsts<T> {
     pub tau_c: T,
 }
 
+impl InitialValues_SatExp_TwoDecExp_SeparateConsts<float> {
+    fn from_vec_vf(params: &ParamsV) -> Self {
+        match params.0.as_slice()[..] {
+            [amplitude_b, amplitude_c, shift, tau_a, tau_b, tau_c] => Self { amplitude_b, amplitude_c, shift, tau_a, tau_b, tau_c },
+            _ => unreachable!()
+        }
+    }
+}
+
 impl<T: Copy> InitialValuesGeneric<T> for InitialValues_SatExp_TwoDecExp_SeparateConsts<T> {
     const LEN: usize = 6;
 
@@ -103,7 +112,17 @@ impl<T: Copy> InitialValuesGeneric<T> for InitialValues_SatExp_TwoDecExp_Separat
     }
 
     fn params_to_points_v(&self, params: &ParamsV, points_len: usize, x_start_end: (float, float)) -> DeconvolvedV {
-        todo!()
+        type SelfF = InitialValues_SatExp_TwoDecExp_SeparateConsts<float>;
+        let SelfF { amplitude_b, amplitude_c, shift, tau_a, tau_b, tau_c } = SelfF::from_vec_vf(params);
+        let mut points = DVect::zeros(points_len);
+        for i in 0..points_len {
+            let x: float = i_to_x(i, points_len, x_start_end);
+            let x_m_shift = x - shift;
+            let y = (1. - exp(-x_m_shift/tau_a)) * (amplitude_b*exp(-x_m_shift/tau_b) + amplitude_c*exp(-x_m_shift/tau_c));
+            let y = y.max(0.);
+            points[i] = y;
+        }
+        DeconvolvedV(points)
     }
 }
 
